f282e469fb594246ec0fc17c937e2b57daef8bab
hariprasathys22 <confluenceinfotechsolutions@gmail.com>
2022-07-09 09:32:01 +0530

    Learnt os and path module

---
+++ 1-module.js
@@ -0,0 +1,11 @@
+
+const names = require('./2-names')
+const sayHi = require('./3-utils')
+require('./5-mind-grenade')
+const generic = require('./4-alernative-flavour');
+console.log(names);
+// sayHi(names.peter);
+// sayHi(names.parker);
+// sayHi("Hp Nation");
+
+console.log(generic);
---
+++ 2-names.js
@@ -0,0 +1,7 @@
+const peter = "peter";
+const parker = "parker";
+
+//local
+const secret = "Super Secret";
+
+module.exports = {peter, parker};
